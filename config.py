9b3d885dd9d473108e811f706108890979e7ef67
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-09-17 12:27:29 +0300

    Update EPIC value to 'EURUSD'

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ DEMO = True
 
 # إعدادات السيرفر والسوق
 SERVER = "https://demo-api-capital.backend-capital.com" if DEMO else "https://api-capital.backend-capital.com"
-EPIC = "CS.D.EURUSD.CFD.IP"
+EPIC = "EURUSD"
 
 
 MAX_DATA_POINTS = 1000

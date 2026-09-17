c6a122aff422bfa5386c701ebc386ecc33e3b527
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-09-17 12:24:20 +0300

    Change EPIC to 'CS.D.EURUSD.TODAY.IP'
    Updated the EPIC variable to use 'CS.D.EURUSD.TODAY.IP'.

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ DEMO = True
 
 # إعدادات السيرفر والسوق
 SERVER = "https://demo-api-capital.backend-capital.com" if DEMO else "https://api-capital.backend-capital.com"
-EPIC = "CS.D.EURUSD.MINI.IP"
+EPIC = "CS.D.EURUSD.TODAY.IP"
+
 MAX_DATA_POINTS = 1000
 
 # 🔄 تم التعديل هنا: تغيير الفريم الزمني إلى الدقيقة الواحدة
